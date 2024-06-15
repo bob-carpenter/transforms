abbf0909ac0f7ccd276865519a3d3f13d3c1536c
Seth Axen <seth@sethaxen.com>
2024-06-15 13:46:16 +0200

    Merge jax transform test sets

--- jax_transforms/tests/test_transforms.py
+++ jax_transforms/tests/test_transforms.py
@@ -34,61 +34,60 @@ expanded_transforms = [
 ]
 
 
-@pytest.mark.parametrize("transform", basic_transforms)
+def _allclose(x, y):
+    if isinstance(x, tuple) and isinstance(y, tuple):
+        return jnp.all(jnp.array([jnp.allclose(xi, yi) for xi, yi in zip(x, y)]))
+    elif isinstance(x, tuple) or isinstance(y, tuple):
+        raise ValueError("x and y must both be tuples or neither")
+    else:
+        return jnp.allclose(x, y)
+
+
+def logdetjac(f):
+    jac = jax.jacobian(f)
+
+    def logdetjac_f(y):
+        J = jac(y)
+        if isinstance(J, tuple):
+            J_r, J_x = J
+            # stack J_r as a vector over J_x[:-1, :]
+            J_square = jnp.concatenate(
+                [jnp.expand_dims(J_r, axis=-2), J_x[..., :-1, :]], axis=-2
+            )
+        else:
+            J_square = J[..., :-1, :]
+        return jnp.linalg.slogdet(J_square)[1]
+
+    return logdetjac_f
+
+
+@pytest.mark.parametrize("transform", basic_transforms + expanded_transforms)
 @pytest.mark.parametrize("N", [3, 5, 10])
 @pytest.mark.parametrize("batch_dims", [(), (3,), (3, 4)])
 @pytest.mark.parametrize("seed", np.random.default_rng(0).integers(0, 1000, 10))
-def test_basic_transform(transform, N, batch_dims, seed):
+def test_transform(transform, N, batch_dims, seed):
     trans = transform()
-    y = jax.random.normal(key=jax.random.key(seed), shape=batch_dims + (N - 1,))
+    M = N if transform in expanded_transforms else N - 1
+    y = jax.random.normal(key=jax.random.key(seed), shape=batch_dims + (M,))
 
+    # check consistency of various methods
     x = trans.constrain(y)
-    assert x.shape[-1] == N
-    assert jnp.all(x >= 0)
-    assert jnp.allclose(jnp.sum(x, axis=-1), 1)
-    assert jnp.allclose(x, vmap_over_leading_axes(trans.constrain, y))
-
     y2 = trans.unconstrain(x)
-    assert jnp.allclose(y2, y)
-
     x2, logJ = trans.constrain_with_logdetjac(y)
-    assert jnp.allclose(x2, x)
-
-    logJ_expected = vmap_over_leading_axes(
-        lambda y: jnp.linalg.slogdet(jax.jacobian(trans.constrain)(y)[:-1, :])[1],
-        y,
-    )
+    assert _allclose(x, vmap_over_leading_axes(trans.constrain, y))
+    assert _allclose(x, x2)
+    assert jnp.allclose(y2, y)
+    is_expanded = isinstance(x, tuple)
+    logJ_expected = vmap_over_leading_axes(logdetjac(trans.constrain), y)
     assert jnp.allclose(logJ, logJ_expected)
 
-
-@pytest.mark.parametrize("transform", expanded_transforms)
-@pytest.mark.parametrize("N", [3, 5, 10])
-@pytest.mark.parametrize("batch_dims", [(), (3,), (3, 4)])
-@pytest.mark.parametrize("seed", np.random.default_rng(0).integers(0, 1000, 10))
-def test_expanded_transform(transform, N, batch_dims, seed):
-    trans = transform()
-    y = jax.random.normal(key=jax.random.key(seed), shape=batch_dims + (N,))
-
-    r_x = trans.constrain(y)
-    x = r_x[..., 1:]
+    # verify basic properties are satisfied
+    if is_expanded:
+        r, x = x
+        assert r.shape == batch_dims
     assert x.shape[-1] == N
     assert jnp.all(x >= 0)
     assert jnp.allclose(jnp.sum(x, axis=-1), 1)
-    assert jnp.allclose(r_x, vmap_over_leading_axes(trans.constrain, y))
-
-    y2 = trans.unconstrain(r_x)
-    assert jnp.allclose(y2, y)
-
-    r_x2, logJ = trans.constrain_with_logdetjac(y)
-    assert jnp.allclose(r_x2, r_x)
-
-    logJ_expected = vmap_over_leading_axes(
-        lambda y: jnp.linalg.slogdet(jax.jacobian(trans.constrain)(y)[:-1, :])[1],
-        y,
-    )
-    assert jnp.allclose(logJ, logJ_expected)
-
-    assert trans.default_prior(r_x).shape == batch_dims
 
 
 @pytest.mark.parametrize("N", [3, 5, 10])

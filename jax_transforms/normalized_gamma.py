007868853449ed8cb511bbc3ac64b485190a5b29
Seth Axen <seth@sethaxen.com>
2024-06-15 12:35:41 +0200

    Add JAX implementation of NormalizedGamma

---
+++ jax_transforms/normalized_gamma.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass
+
+import jax
+import jax.numpy as jnp
+from tensorflow_probability.substrates.jax import bijectors, distributions
+
+from .expanded_softmax import ExpandedSoftmax
+
+
+@dataclass
+class NormalizedGamma:
+    alpha: jax.Array
+
+    @property
+    def distribution(self) -> distributions.Distribution:
+        return distributions.ExpGamma(concentration=self.alpha, rate=1)
+
+    def _dirichlet_log_prob(self, x):
+        return distributions.Dirichlet(self.alpha).log_prob(x)
+
+    def unconstrain(self, r_x):
+        z = ExpandedSoftmax().unconstrain(r_x)
+        return jax.scipy.stats.norm.ppf(self.distribution.cdf(z))
+
+    def constrain(self, y):
+        z = self.distribution.quantile(jax.scipy.stats.norm.cdf(y))
+        r_x = ExpandedSoftmax().constrain(z)
+        return r_x
+
+    def constrain_with_logdetjac(self, y):
+        r_x = self.constrain(y)
+        x = r_x[..., 1:]
+        lp_norm = jnp.sum(jax.scipy.stats.norm.logpdf(y), axis=-1)
+        lp_r = self.default_prior(r_x)
+        lp_dirichlet = self._dirichlet_log_prob(x)
+        logJ = lp_norm - lp_dirichlet - lp_r
+        return r_x, logJ
+
+    def default_prior(self, r_x):
+        N = r_x.shape[-1] - 1
+        r = r_x[..., 0]
+        return distributions.ExpGamma(concentration=N, rate=1).log_prob(r)
+
+
+@dataclass
+class NormalizedExponential(NormalizedGamma):
+    def __init__(self):
+        super().__init__(alpha=jnp.ones(()))
+
+    # the below are just more efficient implementations of the superclass methods
+    @property
+    def distribution(self) -> distributions.Distribution:
+        return distributions.TransformedDistribution(
+            distributions.Exponential(rate=1),
+            bijectors.Log(),
+        )
+
+    def _dirichlet_log_prob(self, x):
+        N = x.shape[-1]
+        return jax.scipy.special.gammaln(N)

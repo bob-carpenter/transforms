d39a9a14968a09bc3649ab2aabac3c686e5d92bf
Seth Axen <seth@sethaxen.com>
2024-06-21 14:58:03 +0200

    Make pyright play well with snakemake scripts

---
+++ workflow/scripts/__builtins__.pyi
@@ -0,0 +1,5 @@
+from typing import Any
+
+# declare snakemake so that pyright doesn't complain about an undefined variable
+# https://github.com/microsoft/pyright/blob/2b0a1d92dda37fb80420e80d1d3933d9287be21f/docs/builtins.md
+snakemake: Any
